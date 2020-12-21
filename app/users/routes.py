72b426c1bde13be8bf0954ae0f1670ebac4bb0dd
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-12-21 23:49:16 +0300

    add endpoints for signup and login

--- app/users/routes.py
+++ app/users/routes.py
@@ -5,3 +5,13 @@ users = Blueprint("Users", __name__)
 @users.route("/", methods=["GET", "POST"])
 def index():
     return render_template("layout.html")
+
+@users.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
