855e187d445e3c6d30bb84a64ee76850cef70115
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-12-21 23:48:45 +0300

    add user model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,10 @@
 from flask_sqlalchemy import Model
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
-    email = 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
