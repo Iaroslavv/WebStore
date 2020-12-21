efaa2dd0bbe70ff5ea05d76971889b83ab693788
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2020-12-22 02:24:55 +0300

    add repr

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from flask_sqlalchemy import Model
 from app import db, login_manager
 from flask_login import UserMixin
 
@@ -7,8 +6,14 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    
+    
+    
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
