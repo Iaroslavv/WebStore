0a92874326d6a05dfc902dc1d45ff0e31842b6a4
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-01-12 11:29:42 +0300

    add unique name

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     
     def get_reset_token(self, expires_sec=1800):
